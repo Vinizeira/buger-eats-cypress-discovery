806ec846f21d421b025c351085ef60463f5cf3c7
Vinizeira <rizaniathecat@gmail.com>
2022-02-02 14:48:17 -0300

    Ajusta a Home para os padrões

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,13 +1,13 @@
 
 describe ('home page', () => {  //Defindo uma switch de teste
     it ('app deve estar online',()=>{ 
-        cy.viewport(1440, 900)
-        var aceitacao =  cy.visit('https://buger-eats-qa.vercel.app/')
+
+        var visitou =  cy.visit('/')
 
         cy.get('#page-home main h1').should('have.text','Seja um parceiro entregador pela Buger Eats') //validar ser a o texto
 
       
-            console.log(aceitacao)
+            console.log(visitou)
     
            
         
